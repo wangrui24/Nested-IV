9c5df59a57487af62fdeccc8a26b1cf0f780a6b8
Rui Wang <47764869+wangrui24@users.noreply.github.com>
2026-01-31 13:49:01 -0800

    Add files via upload

---
+++ run_test1_misspecification.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --array=1-1000
+#SBATCH --nodes=1
+#SBATCH --output=Rout/par-%J.out
+#SBATCH --error=Rout/par-%J.err
+#SBATCH --cpus-per-task=1
+
+## turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=wangrui8@uw.edu
+
+ml fhR/4.0.4-foss-2020b
+
+R CMD BATCH --no-save  cluster_test1_model_misspecification.R Rout6/example_slurm${SLURM_ARRAY_TASK_ID}.Rout
